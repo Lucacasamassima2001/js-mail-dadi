e320310137480c468680f4b85a8500e45ea1de1b
Lucacasamassima2001 <casamassimaluca723@gmail.com>
2023-04-18 10:56:35 +0200

    for added on email

--- gioco_dei_dadi/script.js
+++ gioco_dei_dadi/script.js
@@ -2,16 +2,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+const eleplay = document.querySelector(".play")
+let elescore1 = document.querySelector(".player-score")
+let elescore2 = document.querySelector(".pc-score")
+
+
+
+eleplay.addEventListener("click" , function(){
 let userNumber = parseInt(Math.random()* 6.) + 1;
 console.log(userNumber.toFixed(0))
-
 let pcNumber = parseInt(Math.random()* 6.) + 1;
 console.log(pcNumber.toFixed(0))
-
-
-
 let display = document.getElementById("Who-wins")
-
+elescore1.innerHTML = userNumber
+elescore2.innerHTML = pcNumber 
  if(userNumber > pcNumber){
     console.log("hai vinto utente!")
     display.innerHTML = "HAI VINTO UTENTE!";
@@ -24,3 +28,9 @@ let display = document.getElementById("Who-wins")
         console.log("i numeri si equivalgono")
         display.innerHTML = "I NUMERI SI EQUIVALGONO!";  
     }
+    }
+    
+  )
+
+
+       
--- mail/script.js
+++ mail/script.js
@@ -7,11 +7,38 @@
 
 let usermail = prompt("qual è la tua mail?")
 
-const NamesList = ["michele@gmail.com","laura@gmail.com","luisa@gmail.com","giuseppe@gmail.com","giovanni@gmail.com" ]
+const arrNamesList = [
+    "michele@gmail.com",
+    "laura@gmail.com",
+    "luisa@gmail.com",
+    "giuseppe@gmail.com",
+    "giovanni@gmail.com" 
+];
 // console.log(NamesList)
 // console.log(NamesList.includes(usermail))
-if (NamesList.includes(usermail)){
-    console.log("benvenuto nella tua area personale!")
+
+// METODO INCLUDES
+
+// if (arrNamesList.includes(usermail)){
+//     console.log("benvenuto nella tua area personale!")
+// }else{
+//     console.log("Non risulta alcuna mail con questo indirizzo.")
+// }
+
+
+// METODO FOR
+
+
+let isFound = false
+for(let i = 0; i < arrNamesList.length; i++){
+    
+    if (usermail == arrNamesList[i]){
+    isFound = true
+    }
+}
+
+if(isFound){
+    console.log("email trovata")
 }else{
-    console.log("Non risulta alcuna mail con questo indirizzo.")
+    console.log("email non trovata")
 }
